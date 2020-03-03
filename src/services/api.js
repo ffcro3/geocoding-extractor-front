5e6f235a55c3c9875e14cd71e1b28b5f9ed891fc
Fabricio Rocha <fabriciofrocha87@gmail.com>
2020-03-03 01:49:41 -0300

    AccessControlAlloworigin

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,8 @@ const api = axios.create({
   baseURL: 'https://api.geolocation.girojundiai.com.br',
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
   }
 });
 
